54e82526bf9f5d031da28810a591873266b916c5
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-02-22 10:02:59 +0200

    Remove users as unneeded

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+      {
+        name: "Tuomas"
+      },
+      {
+        name: "Erkki"
+      }
+    ]);
 });
 
 module.exports = router;
